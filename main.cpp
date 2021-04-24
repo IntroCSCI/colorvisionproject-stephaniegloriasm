ff4c1b9a9de076be7b4bd6a1377e7a23107cd6f1
Gloria Sanchez <>
2021-04-24 16:16:30 +0000

    p02

--- main.cpp
+++ main.cpp
@@ -5,8 +5,7 @@
 #include <vector>
 using namespace std;
 
-void printVec(vector<string>);
-bool isPrimary(vector<string>&);
+bool isPrimary(string, const vector<string>&);
 void isUnique(vector<string>&);
 
 int main(){
@@ -17,8 +16,8 @@ int main(){
   size_t position;
  
   cout<<"Please name a new file: \n";
-  
   getline(cin, fileName);
+
   reader.open(fileName, ios::in);
   vector<string>hexadecimalValues;
 
@@ -44,11 +43,13 @@ int main(){
               break;
             }
           }
-          else{
+          else
+          {
 
             if(isPrimary(line.substr(position, 6), hexadecimalValues)==true){
-              hexadecimalValues.push_back(line.substr(position, 6));
-              break;
+                hexadecimalValues.push_back(line.substr(position, 6));
+                break;
+            }
           }
         }
       }
@@ -62,14 +63,14 @@ int main(){
     cout << '#' << hexadecimalValues[counter] <<endl;
   }
   
-  cout<<"These are primary colors:";
+  cout<<"These are  colors:";
   isUnique(hexadecimalValues);
 
   return 0;
 }
 
 
-bool isPrimary(string hColor, vector<string> & primaryColor){
+bool isPrimary(string hColor, const vector<string> & primaryColor){
   for(int pC=0; pC<primaryColor.size();pC++){
     if(primaryColor[pC]==hColor)
     {
@@ -79,25 +80,25 @@ bool isPrimary(string hColor, vector<string> & primaryColor){
   return true;
 }
 
-void isUnique(vector <string> & hexadecimalValues){
-  for( int uC=0;uC<hexadecimalValues.size();uC++){
+void isUnique(vector <string> & hColor){
+  for( int uC=0;uC<hColor.size();uC++){
 
-    if(hexadecimalValues[uC]=="000"){
-      hexadecimalValues[uC]=="000000";
+    if(hColor[uC]=="000"){
+      hColor[uC]=="000000";
     }
-    else if(hexadecimalValues[uC]=="ffffff"){
-      hexadecimalValues[uC]=="ffffff";
+    else if(hColor[uC]=="ffffff"){
+      hColor[uC]=="ffffff";
     }
-    else if(hexadecimalValues[uC]=="ff"){
-      hexadecimalValues[uC]=="ff0000";
+    else if(hColor[uC]=="ff"){
+      hColor[uC]=="ff0000";
     }
-    else if(hexadecimalValues[uC]=="fff"){
-      hexadecimalValues[uC]=="fff000";
+    else if(hColor[uC]=="fff"){
+      hColor[uC]=="fff000";
     }
-    else if(hexadecimalValues[uC]=="000fff"){
-      hexadecimalValues[uC]=="000fff";
+    else if(hColor[uC]=="000fff"){
+      hColor[uC]=="000fff";
     }
-    cout<<"#"<<hexadecimalValues[uC]<<endl;
+    cout<<"#"<<hColor[uC]<<endl;
   }
   cout << "Those are some nice colors! Please, run the code again to enter a new file!";
 }
